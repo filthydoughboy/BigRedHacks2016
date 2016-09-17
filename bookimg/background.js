a8be2d8de93cb6a28c45f1431ea098137e3f7825
kohyujin1436 <yk583@cornell.edu>
2016-09-17 19:35:50 -0400

    img working

--- bookimg/background.js
+++ bookimg/background.js
@@ -53,9 +53,49 @@ chrome.runtime.onMessage.addListener(
     switch (request.directive) {
     case "screenshot":
       getCurrentTabUrl(function(url) {
-        chrome.tabs.executeScript({file: 'dialog.js'}, function() {
-          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {current_url: url});
+
+        var id = 100;
+        chrome.tabs.captureVisibleTab(function(screenshotUrl) {
+          var imageUrl = screenshotUrl;
+          var targetId = null;
+
+          chrome.tabs.onUpdated.addListener(function listener(tabId, changedProps) {
+            // We are waiting for the tab we opened to finish loading.
+            // Check that the tab's id matches the tab we opened,
+            // and that the tab is done loading.
+            if (tabId != targetId || changedProps.status != "complete")
+              return;
+
+            // Passing the above test means this is the event we were waiting for.
+            // There is nothing we need to do for future onUpdated events, so we
+            // use removeListner to stop getting called when onUpdated events fire.
+            chrome.tabs.onUpdated.removeListener(listener);
+
+            // Look through all views to find the window which will display
+            // the screenshot.  The url of the tab which will display the
+            // screenshot includes a query parameter with a unique id, which
+            // ensures that exactly one view will have the matching URL.
+            var views = chrome.extension.getViews();
+            for (var i = 0; i < views.length; i++) {
+              var view = views[i];
+              if (view.location.href == imageUrl) {
+                view.setScreenshotUrl(screenshotUrl);
+                break;
+              }
+            }
+          });
+
+          // chrome.tabs.create({url: imageUrl}, function(tab) {
+          //   targetId = tab.id;
+          // });
+
+          chrome.tabs.executeScript({file: 'dialog.js'}, function() {
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+              chrome.tabs.sendMessage(tabs[0].id, {
+                current_url: url,
+                img_url: imageUrl
+              });
+            });
           });
         });
       });
--- bookimg/dialog.js
+++ bookimg/dialog.js
@@ -12,20 +12,29 @@ chrome.runtime.onMessage.addListener(
   function(message, sender, sendResponse) {
     var dialog = document.createElement("dialog");
     dialog.textContent = message.current_url;
+    document.body.appendChild(dialog);
+    
     var save_button = document.createElement("button");
-    var close_button = document.createElement("button");
     save_button.textContent = "Save";
-    close_button.textContent = "Close";
     dialog.appendChild(save_button);
-    dialog.appendChild(close_button);
     save_button.addEventListener("click", function() {
       onSave();
       dialog.close();
     })
+    
+    var close_button = document.createElement("button");
+    close_button.textContent = "Close";
+    dialog.appendChild(close_button);
     close_button.addEventListener("click", function() {
       dialog.close();
     })
-    document.body.appendChild(dialog);
+
+    var image = document.createElement("img");
+    image.src = message.img_url;
+    image.style.width = "500px";
+    image.style.height = "300px";
+    dialog.appendChild(image);
+
     dialog.showModal();
   }
 )
