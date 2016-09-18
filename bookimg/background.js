91de51f265ad5ebb29c39dcf77ea20e52632df40
kohyujin1436 <yk583@cornell.edu>
2016-09-17 20:47:22 -0400

    style changes to dialog.js

--- bookimg/background.js
+++ bookimg/background.js
@@ -85,10 +85,6 @@ chrome.runtime.onMessage.addListener(
             }
           });
 
-          // chrome.tabs.create({url: imageUrl}, function(tab) {
-          //   targetId = tab.id;
-          // });
-
           chrome.tabs.executeScript({file: 'dialog.js'}, function() {
             chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
               chrome.tabs.sendMessage(tabs[0].id, {
--- bookimg/dialog.js
+++ bookimg/dialog.js
@@ -11,12 +11,35 @@ function onSave() {
 chrome.runtime.onMessage.addListener(
   function(message, sender, sendResponse) {
     var dialog = document.createElement("dialog");
-    dialog.textContent = message.current_url;
+    dialog.style.width = '500px';
+    dialog.style.height = '700px';
     document.body.appendChild(dialog);
+
+    var dialog_div = document.createElement("div");
+    dialog_div.style.display = 'flex';
+    dialog_div.style['flex-direction'] = 'column';
+    dialog.appendChild(dialog_div);
     
+    var image = document.createElement("img");
+    image.src = message.img_url;
+    image.style.width = "400px";
+    image.style.height = "300px";
+    dialog_div.appendChild(image);
+
+    var title_input = document.createElement("input");
+    title_input.type = "text";
+    title_input.name = "Title";
+    title_input.value = "Please insert a title";    
+    dialog_div.appendChild(title_input);
+
+    var description_input = document.createElement("input");
+    description_input.type = "text";
+    description_input.name = "Title";
+    description_input.value = "Please insert a description"; 
+    dialog_div.appendChild(description_input);
+
     var save_button = document.createElement("button");
     save_button.textContent = "Save";
-    dialog.appendChild(save_button);
     save_button.addEventListener("click", function() {
       onSave();
       dialog.close();
@@ -24,18 +47,22 @@ chrome.runtime.onMessage.addListener(
     
     var close_button = document.createElement("button");
     close_button.textContent = "Close";
-    dialog.appendChild(close_button);
     close_button.addEventListener("click", function() {
       dialog.close();
     })
 
-    var image = document.createElement("img");
-    image.src = message.img_url;
-    image.style.width = "500px";
-    image.style.height = "300px";
-    dialog.appendChild(image);
+    var button_panel = document.createElement("div");
+    button_panel.appendChild(save_button);
+    button_panel.appendChild(close_button);
+    button_panel.style.display = 'flex';
+    button_panel.style['flex-direction'] = 'row';
 
+    dialog_div.appendChild(button_panel);
     dialog.showModal();
   }
-)
+);
+
+$(window).resize(function() {
+  $("#dialog").dialog("option", "position", "center");
+});
 
